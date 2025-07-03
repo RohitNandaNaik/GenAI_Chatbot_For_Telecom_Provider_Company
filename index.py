f5ab3d017b2aed9eeb8ff9467c4142a7ea7589f2
RohitNandaNaik <2001rohitnaik1234@gmail.com>
2025-07-03 11:48:18 +0530

    index updated

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-# hello
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Welcome to Telecom Support Chatbot!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
